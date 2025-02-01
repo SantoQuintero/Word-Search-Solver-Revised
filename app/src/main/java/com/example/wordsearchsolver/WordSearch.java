068f0812f18ce1ffaf48cfa335a00ceb6d95a1dc
SantoQuintero <117685544+SantoQuintero@users.noreply.github.com>
2025-01-31 22:23:53 -0800

    Update WordSearch.java

--- app/src/main/java/com/example/wordsearchsolver/WordSearch.java
+++ app/src/main/java/com/example/wordsearchsolver/WordSearch.java
@@ -52,7 +52,6 @@ public class WordSearch {
         for(String s : wordSearchAnswers)
         {
             boolean found = false;
-            System.out.println("\n\n\t\t\tChecking: " + s+"\n\n");
             for(int i = 0; i < wordSearchLetters.length&&!found;i++)
             {
                 for(int j = 0; j<wordSearchLetters[i].length&&!found;j++)
@@ -60,146 +59,93 @@ public class WordSearch {
                     if(wordSearchLetters[i][j].equalsIgnoreCase(s.substring(0,1)))
                     {
                         //if it is then it contiues to check the letter next to this letter to see if the next letter of answer word appears
-//                        System.out.println("\n\n\nIf block has succeeded and now we are at:\ni: "
-//                        + i + "\tj: " + j+"\n Both are apparently the first of " + s +", letter " +s.substring(0,1));
                         int count = 0;
                         while((count<s.length() && inBounds(i,j+count).equalsIgnoreCase(s.substring(count,count+1))))
                         {
-//                            System.out.println("horiz------------I am in while\n inBounds gets: "
-//                            + inBounds(i,j+count)  +"\nSubstring of count-count+1 gets: "+ s.substring(count,count+1));//this could be trouble
-
                             //once the last letter is found x spaces away from the first in
                             //the direction of the chain of letters, they are capitalized to indicate the location
                             if(count==s.length()-1 && wordSearchLetters[i][j+count].equalsIgnoreCase(s.substring(s.length()-1)))
                             {
                                 //going to the right horizontally because the column number is increasing
-                                System.out.println("Got here---------------lastlastlasst");
                                 found=true;
                                 for(int back = count; back>-1;back--)
                                 {
-
-                                    System.out.println("Got here--------------------------before uppercase");
                                     wordSearchLetters[i][j+back] = wordSearchLetters[i][j+back].toUpperCase();
                                     wordsFound.add(s);
                                 }
                             }
                             count++;
-//                          System.out.println("Count for while is: " + count);
                         }
-//                        System.out.println("Entered?...Count: " + count+"\n inBounds gets: "
-//                        + inBounds(i,j+count)  +"\nSubstring of count-count+1 gets: danger"/*+ s.substring(count,count+1)*/);
 
                         //up and down
                         //the aforementioned process is done more than once because the program
-                        //needs to check if it is diagnol, horizontal, vertical, etc.
-
+                        //needs to check if it is diagonal, horizontal, vertical,
                         count = 0;
                         while((count<s.length() && inBounds(i+count,j).equalsIgnoreCase(s.substring(count,count+1))))
                         {
-//                            System.out.println("Vert------------inside while\n inBounds gets: "
-//                            + inBounds(i+count,j)  +"\nSubstring of count-count+1 gets: "+ s.substring(count,count+1));
-//                            System.out.println("\n\n\nGOT HERE\n"+s.substring(count,count+1));
                             if(count==s.length()-1 &&wordSearchLetters[i+count][j].equalsIgnoreCase(s.substring(s.length()-1)))
                             {
                                 //going to the down vertically because the row number is increasing
-//                              System.out.println("Got here---------------lastlastlasst");
                                 found=true;
                                 for(int back = count; back>-1;back--)
                                 {
-
-                                    System.out.println("Got here--------------------------before uppercase");
                                     wordSearchLetters[i+back][j] = wordSearchLetters[i+back][j].toUpperCase();
                                     wordsFound.add(s);
                                 }
                             }
                             count++;
-//                          System.out.println("Count:(break) " + count);
                         }
-//                        System.out.println("Count(1) = no enter... Count: " + count+"\n inBounds gets: "
-//                        + inBounds(i +count,j)  +"\nSubstring of count-count+1 gets: "/*+ s.substring(count,count+1)*/);
+                        
                         count = 0;
                         while((count<s.length() && inBounds(i-count,j).equalsIgnoreCase(s.substring(count,count+1))))
                         {
-                            //     System.out.println("Count(1) = no enter... Count: " + count+"\n inBounds gets: "
-                            //     + inBounds(i-count,j)  +"\nSubstring of count-count+1 gets: "+ s.substring(count,count+1));
-                            //     System.out.println("\n\\n\n\n\n\n\nGOT HERE\n\n\n\n\n\n\n"+s.substring(count,count+1));
                             if(count==s.length()-1 &&wordSearchLetters[i-count][j].equalsIgnoreCase(s.substring(s.length()-1)))
                             {
-                                //going to the up vertically because the row number is decreasing
-                                // System.out.println("Got here---------------lastlastlasst");
+                                //going up vertically because the row number is decreasing
                                 found=true;
                                 for(int back = count; back>-1;back--)
                                 {
-
-                                    // System.out.println("Got here--------------------------before uppercase");
                                     wordSearchLetters[i-back][j] = wordSearchLetters[i-back][j].toUpperCase();
                                     wordsFound.add(s);
                                 }
                             }
                             count++;
-                            //System.out.println("Count:(break) " + count);
                         }
-//                        System.out.println("Count(1) = no enter... Count: " + count+"\n inBounds gets: "
-//                        + inBounds(i-count,j)  +"\nSubstring of count-count+1 gets: "/*+ s.substring(count,count+1)*/);
 
-
-                        // //diagonally both//
                         count = 0;
                         while((count<s.length() && inBounds(i+count,j+count).equalsIgnoreCase(s.substring(count,count+1))))
                         {
-                            //     System.out.println("Count(1) = no enter... Count: " + count+"\n inBounds gets: "
-                            // + inBounds(i+count,j+count)  +"\nSubstring of count-count+1 gets: "+ s.substring(count,count+1));
-                            //     System.out.println("\n\\n\n\n\n\n\nGOT HERE\n\n\n\n\n\n\n"+s.substring(count,count+1));
                             if(count==s.length()-1 &&wordSearchLetters[i+count][j+count].equalsIgnoreCase(s.substring(s.length()-1)))
                             {
                                 //going down diagonally because the column and row number is increasing
-                                //System.out.println("Got here---------------lastlastlasst");
                                 found=true;
                                 for(int back = count; back>-1;back--)
                                 {
-
-                                    //System.out.println("Got here--------------------------before uppercase");
                                     wordSearchLetters[i+back][j+back] = wordSearchLetters[i+back][j+back].toUpperCase();
                                     wordsFound.add(s);
                                 }
                             }
                             count++;
-                            //System.out.println("Count:(break) " + count);
                         }
-                        // System.out.println("Count(1) = no enter... Count: " + count+"\n inBounds gets: "
-                        // + inBounds(i +count,j+count)  +"\nSubstring of count-count+1 gets: "/*+ s.substring(count,count+1)*/);
-
+                        
                         count = 0;
                         while((count<s.length() && inBounds(i-count,j+count).equalsIgnoreCase(s.substring(count,count+1))))
                         {
-                            // System.out.println("Count(1) = no enter... Count: " + count+"\n inBounds gets: "
-                            // + inBounds(i-count,j+count)  +"\nSubstring of count-count+1 gets: "+ s.substring(count,count+1));
-                            // System.out.println("\n\\n\n\n\n\n\nGOT HERE\n\n\n\n\n\n\n"+s.substring(count,count+1));
                             if(count==s.length()-1 &&wordSearchLetters[i-count][j+count].equalsIgnoreCase(s.substring(s.length()-1)))
                             {
                                 //going up diagonally because the column number is increasing and the row number is decreasing
-                                // System.out.println("Got here---------------lastlastlasst");
                                 found=true;
                                 for(int back = count; back>-1;back--)
                                 {
-
-                                    // System.out.println("Got here--------------------------before uppercase");
                                     wordSearchLetters[i-back][j+back] = wordSearchLetters[i-back][j+back].toUpperCase();
                                     wordsFound.add(s);
                                 }
                             }
                             count++;
-                            //System.out.println("Count:(break) " + count);
                         }
-                        System.out.println("Count(1) = no enter... Count: " + count+"\n inBounds gets: "
-                                + inBounds(i -count,j+count)  +"\nSubstring of count-count+1 gets: "/*+ s.substring(count,count+1)*/);
                     }
                 }
             }
-            // findStartLetter();
-            // checkHoriz();
-            // checkVert();
-            // checkDiag();
         }
     }//end of solve
 
@@ -261,12 +207,10 @@ public class WordSearch {
                 if(i+1>=rows && j+1>=count)
                 {
                     wordSearchRefined[i][j] = rawWS.substring(rawWS.length()-1);
-                    //System.out.println(wordSearchRefined[i][j] + i + ", " + j);
                 }
                 else
                 {
                     wordSearchRefined[i][j] = rawWS.substring(allLetters, allLetters+1);
-                    //System.out.println(wordSearchRefined[i][j]);
                 }
 
                 allLetters++;
